adce73e11690ea61a0ca0c1bbee3299203d58267
Eitan Fedenko <eitan.lp@gmail.com>
2021-08-05 23:09:22 +0300

    Deployment fixes

--- scripts/generate-secret.py
+++ scripts/generate-secret.py
@@ -1,6 +1,7 @@
+import os
 from secrets import token_bytes
 
 key = token_bytes(64)
-key_file = open("../keys/secret.txt", "wb")
+key_file = open(os.path.dirname(os.path.realpath(__file__)) + "/keys/secret.txt", "wb")
 key_file.write(key)
 key_file.close()
