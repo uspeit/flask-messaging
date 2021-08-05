82c967e2a59b5b64261746a2907dea479c4eab2e
Eitan Fedenko <eitan.lp@gmail.com>
2021-08-05 23:17:42 +0300

    Secret key script fix

--- scripts/generate-secret.py
+++ scripts/generate-secret.py
@@ -2,7 +2,7 @@ import os
 from secrets import token_bytes
 
 key = token_bytes(64)
-key_dir = os.path.dirname(os.path.realpath(__file__)) + "/keys"
+key_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'keys'))
 os.mkdir(key_dir)
 key_file = open(key_dir + "/secret.txt", "wb")
 key_file.write(key)
