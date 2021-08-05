8cc6a9504dbd1c412e6b419914c0ff26522236cc
Eitan Fedenko <eitan.lp@gmail.com>
2021-08-05 18:11:24 +0300

    Added some more comments

--- auth.py
+++ auth.py
@@ -7,9 +7,12 @@ from models.user import User
 def authenticate(username, password):
     user = User.query.filter_by(username=username).first()
     if user:
+        # Obtain user hash and user salt
         [user_salt, user_hash] = user.password_hash.split(":")
+        # Hash password candidate with user salt
         [_, pass_hash_bytes] = hash_password(password, user_salt)
         pass_hash = encode_hash(pass_hash_bytes)
+        # Compare hash digests
         if hmac.compare_digest(user_hash, pass_hash):
             return user
 
--- encryption.py
+++ encryption.py
@@ -6,9 +6,11 @@ from secrets import token_bytes
 # Create PBKDF2 hash, returns salt, hash
 def hash_password(password, salt_str=None):
     if not salt_str:
+        # Generate unique salt
         salt_bytes = token_bytes(64)
         salt_str = base64.b64encode(salt_bytes).decode('unicode_escape')
     else:
+        # Decode provided salt string
         salt_bytes = base64.b64decode(salt_str.encode('unicode_escape'))
     pass_hash_bytes = hashlib.pbkdf2_hmac('sha256', password.encode('unicode_escape'), salt_bytes, 100000)
     return salt_str, pass_hash_bytes
